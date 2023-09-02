89e5045bed2137b071e2937853bfef890cfeb432
IJE2704 <mh1669101@gmail.com>
2023-09-02 17:00:28 +0600

    practice problem

---
+++ candy-division.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	   int candies;
+	   cin >> candies;
+	   if(candies%3) cout << "NO" << endl;
+	   else cout << "YES" << endl;
+	}
+	return 0;
+}
+
