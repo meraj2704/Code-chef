c361355807366d499422a8d132630c07ee299969
IJE2704 <mh1669101@gmail.com>
2023-09-03 16:16:55 +0600

    practice proble,

---
+++ cheif-play-ludo.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	   int d;
+	   cin >> d;
+	   if(d==6) cout << "yes" << endl;
+	   else cout << "No" << endl;
+	}
+	return 0;
+}
+
