5f8a677a18ce5f1c09477dde7f0a96150d59129e
IJE2704 <mh1669101@gmail.com>
2023-09-04 16:22:56 +0600

    practice problem

---
+++ watching-movie-2x.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	   int x , y;
+	   cin >> x >> y;
+	   int new_time = y/2;
+	   int remaing_time = x -y;
+	   cout << new_time + remaing_time << endl;
+	
+	return 0;
+}
+
