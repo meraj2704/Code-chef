3b4c5f7c343274db080f4305ff5004104df38ee0
IJE2704 <mh1669101@gmail.com>
2023-09-10 20:01:21 +0600

    solve the bike and cars problem

---
+++ cars-and-bikes.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	   int n;
+	   cin >> n;
+	   if(n%4) cout << "YES" << endl;
+	   else cout << "NO" << endl;
+	}
+	return 0;
+}
+
