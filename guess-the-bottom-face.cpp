9c47a4dc6f02b4801f1aa5c294f672ff9172bbfc
IJE2704 <mh1669101@gmail.com>
2023-09-11 06:02:51 +0600

    guess the bottom face problem solve

---
+++ guess-the-bottom-face.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	   int dice;
+	   cin >> dice;
+	   cout << 7 - dice << endl;
+	}
+	return 0;
+}
+
